f9dc48e5fe51208aaaf79815cd5147245dc5b755
TimHyatt707 <shoruye38@hotmail.com>
2017-10-19 09:54:37 -0700

    solved set1-1

---
+++ set1/app.js
@@ -0,0 +1,10 @@
+function hexTo64(string) {
+  let base64String = string;
+  base64String = Buffer.from(base64String, 'hex');
+  return base64String.toString('base64');
+}
+console.log(
+  hexTo64(
+    '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+  )
+);
