145422c639f8203893cb12ba6e4ab7d028c9e5c8
TimHyatt707 <timhyatt707@gmail.com>
2017-10-20 09:32:33 -0700

    solved fixedXOR

--- set1/fixedXOR.js
+++ set1/fixedXOR.js
@@ -1,5 +1,14 @@
-function fixedXOR(string) {
-  let hexed = Buffer.from(string, "hex");
-  hexed = hexed.toString("base64");
+function fixedXOR(string1, string2) {
+  let array1Buffer = Buffer.from(string1, "hex");
+  let array2Buffer = Buffer.from(string2, "hex");
+  let result = array1Buffer.map((element, index) => {
+    return element ^ array2Buffer[index];
+  });
+  return result.toString("hex");
 }
-console.log(fixedXOR("1c0111001f010100061a024b53535009181c"));
+console.log(
+  fixedXOR(
+    "1c0111001f010100061a024b53535009181c",
+    "686974207468652062756c6c277320657965"
+  )
+);
